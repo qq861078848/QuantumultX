3ddce0b789cad97a7f9f3350af3bdf81e17c584b
cyy <861078848@qq.com>
2021-08-14 17:01:16 +0800

    test

--- js/lls.js
+++ js/lls.js
@@ -2,8 +2,8 @@ let body = $response.body
 body = JSON.parse(body)
 
 
-body['darwin']['bought'] = true
-console.log($request.method, $request.url, $request.path, $request.headers)
+body['darwin']['bought'] = true;
+console.log($request.method, $request.url, $request.path, $request.headers);
 $notify("title", "123", "content");
 body = JSON.stringify(body)
 $done({ body })
