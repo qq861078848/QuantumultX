c0f314fe633b36af0a524d06f83276589d64e1bb
cyy <861078848@qq.com>
2021-08-14 17:55:20 +0800

    test

--- js/lls.js
+++ js/lls.js
@@ -1,12 +1,14 @@
 let body = $response.body
+let path = $request.path
 body = JSON.parse(body)
 
-
-body['darwin']['bought'] = true;
-console.log($request.method);
-console.log($request.path);
-console.log($request.url);
-console.log($request.headers);
+console.log(path);
+if (path === "/api/v1/caen/package"){
+    body['darwin']['bought'] = true;
+    for(var key in body){
+        console.log(key)
+    }
+}
 
 $notify("title", "123", "content");
 body = JSON.stringify(body)
