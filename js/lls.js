c46a0642800e124f07bdcc3d06f8767e6b1d991a
cyy <861078848@qq.com>
2021-08-14 19:41:14 +0800

    test

--- js/lls.js
+++ js/lls.js
@@ -34,10 +34,10 @@ if (path === "/api/v1/bell/mine"){
     body["expired_at_sec"] = 1888888888;
 }
 
-if (path === "/api/v1/caen/user_courses/elite"){
-    body_str = '{"courses":[{"courseType":1,"linkUrl":"overlord://to_darwin_core","coverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1587985406120-2.jpg","thumbCoverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1587628391729-2.png","mainTitle":"懂你英语®A⁺","subtitle":"综合系统课","subscriptionInfo":{"subscriptionStatus":1,"expiresAt":188888888},"learningProgress":{"style":1,"current":0,"end":30},"updatedAt":1628867057,"resourceId":""},{"courseType":4,"linkUrl":"overlord://to_bell","coverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1583327786272-4.png","thumbCoverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1584532924339-2.png","mainTitle":"流利说·发音","subtitle":"发音专项课","subscriptionInfo":{"subscriptionStatus":1,"expiresAt":1888888888},"learningProgress":null,"updatedAt":1620006382,"resourceId":""}]}';
-    body = JSON.parse(body_str);
-}
+// if (path === "/api/v1/caen/user_courses/elite"){
+//     body_str = '{"courses":[{"courseType":1,"linkUrl":"overlord://to_darwin_core","coverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1587985406120-2.jpg","thumbCoverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1587628391729-2.png","mainTitle":"懂你英语®A⁺","subtitle":"综合系统课","subscriptionInfo":{"subscriptionStatus":1,"expiresAt":188888888},"learningProgress":{"style":1,"current":0,"end":30},"updatedAt":1628867057,"resourceId":""},{"courseType":4,"linkUrl":"overlord://to_bell","coverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1583327786272-4.png","thumbCoverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1584532924339-2.png","mainTitle":"流利说·发音","subtitle":"发音专项课","subscriptionInfo":{"subscriptionStatus":1,"expiresAt":1888888888},"learningProgress":null,"updatedAt":1620006382,"resourceId":""}]}';
+//     body = JSON.parse(body_str);
+// }
 
 
 $notify("title", "123", "content");
