76500e96f3d6e19c086d280c1feec4976dcb958b
cyy <861078848@qq.com>
2021-08-14 18:30:14 +0800

    test

--- js/lls.js
+++ js/lls.js
@@ -7,12 +7,18 @@ if (path === "/api/v1/caen/package"){
     for(var key in body){
         if(body[key].hasOwnProperty('bought')){
             body[key]["bought"] = true;
+            body[key]["expired"] = false;
+            body[key]["expiredAtSec"] = 1888888888;
+            body[key]["expiresAt"] = 1888888888;
+
         }
         if(key === "darwin"){
             for(var key2 in body["darwin"]){
                 if(body["darwin"][key2].hasOwnProperty('bought')){
                     body["darwin"][key2]["bought"] = true;
                     body["darwin"][key2]["subscriptionStatus"] = 1;
+                    body["darwin"][key2]["expiredAtSec"] = 1888888888;
+                    body["darwin"][key2]["effectedAtSec"] = 1588888888;
                 }
             }
         }
