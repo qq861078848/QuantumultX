78a6f9c027c4f408347bdd3af2d1f3bc7acd12b6
cyy <861078848@qq.com>
2021-08-14 21:17:11 +0800

    test

--- js/lls.js
+++ js/lls.js
@@ -1,8 +1,8 @@
 let body = $response.body
 let path = $request.path
-body = JSON.parse(body)
 
-console.log(path);
+body = JSON.parse(body);
+
 if (path === "/api/v1/caen/package"){
     for(var key in body){
         if(body[key].hasOwnProperty('bought')){
@@ -27,7 +27,6 @@ if (path === "/api/v1/caen/package"){
     }
 }
 
-
 if (path === "/api/v1/bell/mine"){
     body["status"] = 2;
     body["started_at_sec"] = 1600000000;
@@ -36,6 +35,13 @@ if (path === "/api/v1/bell/mine"){
 
 if (path === "/api/v1/caen/user_courses/elite"){
     body_str = '{"courses":[{"courseType":1,"linkUrl":"overlord://to_darwin_core","coverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1587985406120-2.jpg","thumbCoverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1587628391729-2.png","mainTitle":"懂你英语®A⁺","subtitle":"综合系统课","subscriptionInfo":{"subscriptionStatus":1,"expiresAt":188888888},"learningProgress":{"style":1,"current":0,"end":30},"updatedAt":1628867057,"resourceId":""},{"courseType":4,"linkUrl":"overlord://to_bell","coverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1583327786272-4.png","thumbCoverUrl":"https://cdn.llscdn.com/fe-static/yy/image/rc-upload-1584532924339-2.png","mainTitle":"流利说·发音","subtitle":"发音专项课","subscriptionInfo":{"subscriptionStatus":1,"expiresAt":1888888888},"learningProgress":null,"updatedAt":1620006382,"resourceId":""}]}';
+    body = JSON.parse(body_str);
+}
+
+if (path === "/api/v1/ncc/session_reviews"){
+    console.log(path);
+    body_str = JSON.stringify(body);
+    body_str = body_str.replace(/'"unlock":false'/g, '"unlock":true')
     body = JSON.parse(body_str);
 }
 
