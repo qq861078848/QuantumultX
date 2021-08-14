671350f0da4ea02a9a8af207fdd9cd25b04c8f52
cyy <861078848@qq.com>
2021-08-14 18:13:18 +0800

    test

--- js/lls.js
+++ js/lls.js
@@ -8,8 +8,10 @@ if (path === "/api/v1/caen/package"){
         if(body[key].hasOwnProperty('bought')){
             body[key]["bought"] = true;
         }
-        if(body[key] ==="darwin"){
+        if(body[key] === "darwin"){
+            console.log(body[key])
             for(var key2 in body["darwin"]){
+                console.log(body[key][key2])
                 if(body["darwin"][key2].hasOwnProperty('bought')){
                     body["darwin"][key2]["bought"] = true;
                 }
